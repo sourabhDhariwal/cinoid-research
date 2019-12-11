d7d80c6d6f3d3009d59590ec7ab7c4d5f935739e
sourabhDhariwal <sourabh.dhariwal9094@gmail.com>
2019-12-12 01:54:52 +0530

    Updates

---
+++ precache-manifest.a013081bc4ccd364a030d0670fe18fdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12300dae5dd3d7a539d67622cf7d9119",
+    "url": "/cinoid_research/index.html"
+  },
+  {
+    "revision": "1724a8d4587ce9a47e5c",
+    "url": "/cinoid_research/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81ef2b561f6bbd2d2b0a",
+    "url": "/cinoid_research/static/js/2.34256180.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cinoid_research/static/js/2.34256180.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1724a8d4587ce9a47e5c",
+    "url": "/cinoid_research/static/js/main.f13f182b.chunk.js"
+  },
+  {
+    "revision": "09aa35acc4fefbcb823f",
+    "url": "/cinoid_research/static/js/runtime-main.c1a84c3d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cinoid_research/static/media/logo.5d5d9eef.svg"
+  }
+]);
